3560acc25dcb470505ea22c92f6f50f44c0da6f3
Wiktor Przybylski <wikprzybylski@gmail.com>
2020-11-29 14:07:43 +0100

    changed sleep, added ommiting certain types of transactions

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 import pickle
 import time
 import yaml
+import re
 
 from datetime import datetime
 from parseTxt import getEmoji, addNewlines
@@ -22,6 +23,8 @@ apiToken = config['apiToken']
 
 feedQueryUrl = config['feedQueryUrl']
 phidQueryUrl = config['phidQueryUrl']
+
+omitPattern = re.compile(r"PHID-(PROJ|USER)-[a-z0-9]{20}")
 
 try:
     with open('lastChrono', 'rb') as fp:
@@ -44,22 +47,35 @@ q = json.loads(r.content)
 
 telegramPayload = ""
 
+print(len(q['result']))
 
+ommitedMessages = 0
 
-for i in reversed(list(q['result'])): 
-	print(q['result'][i]['text'])
-	currChrono = int(q['result'][i]['chronologicalKey'])
-	if currChrono > lastChrono:
-		lastChrono = currChrono
-	objectId = q['result'][i]['objectPHID']
-	objectTxt = q['result'][i]['text']
-	objectTime = datetime.fromtimestamp(q['result'][i]['epoch']).strftime("%Y-%m-%d %I:%M:%S")
-	phidinfo = requests.get(phidQueryUrl, data={'api.token':apiToken,'phids[0]':objectId})
-	objectUri = phidinfo.json()['result'][objectId]['uri']
-	print(objectUri)
-	telegramPayload = getEmoji(objectTxt) + " " + addNewlines(objectTxt) + "\n\n\U0001F517 Link: " + objectUri + "\n\U0001F4C5 Kiedy: " + objectTime
-	if(len(q['result']) > 100):
-		time.sleep(0.5)
-	tb.send_message(chatId, telegramPayload)
-	with open('lastChrono', 'wb') as fp:
-		pickle.dump(lastChrono, fp)
+if q['result']:
+	for i in reversed(list(q['result'])): 
+		objectId = q['result'][i]['objectPHID']
+		objectTxt = q['result'][i]['text']
+		if omitPattern.match(objectId):
+			print("Do wywalenia")
+			ommitedMessages = ommitedMessages + 1 
+		else:
+			print(q['result'][i]['text'])
+			currChrono = int(q['result'][i]['chronologicalKey'])
+			if currChrono > lastChrono:
+				lastChrono = currChrono
+			objectTime = datetime.fromtimestamp(q['result'][i]['epoch']).strftime("%Y-%m-%d %I:%M:%S")
+			phidinfo = requests.get(phidQueryUrl, data={'api.token':apiToken,'phids[0]':objectId})
+			print(phidinfo.status_code)
+			objectUri = phidinfo.json()['result'][objectId]['uri']
+			print(objectUri)
+			telegramPayload = getEmoji(objectTxt) + " " + addNewlines(objectTxt) + "\n\n\U0001F517 Link: " + objectUri + "\n\U0001F4C5 Kiedy: " + objectTime + "\n\n" + objectId
+			if(len(q['result']) > 80):
+				time.sleep(2)
+			tb.send_message(chatId, telegramPayload)
+			with open('lastChrono', 'wb') as fp:
+				pickle.dump(lastChrono, fp)
+	if ommitedMessages > 0:
+		tb.send_message(chatId, "Pominiętych wiadomości: " + str(ommitedMessages) + " - Sprawdź na https://phabricator.hskrk.pl/feed.") 
+
+else:
+	print("Brak nowych wiadomosci")
